849b126b16d06d2d5abae5721ae4fdca5dc82852
Igor Blumberg <igor@dimona.com.br>
2016-04-13 15:35:28 -0300

    new method

--- PhpSigepFPDF.php
+++ PhpSigepFPDF.php
@@ -4,4 +4,10 @@ require_once __DIR__ . '/fpdf.php';
 
 class PhpSigepFPDF extends FPDF
 {
+    function Output($name='', $dest='')
+    {
+        $result = parent::Output($name,$dest);
+        stream_wrapper_unregister("var");//isso permite que seja gerado dois PDFs na mesma requisição, útil se você quiser gerar a PLP e as etiquetas de uma só vez.
+        return $result;
+    }
 }
